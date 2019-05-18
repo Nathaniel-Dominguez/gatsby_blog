08e21f0f9f2c92f8ef0b8bb3e8e665c07e9e7977
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-05-18 14:41:41 -0700

    created an about page with some pirate ipsum

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const AboutPage = () => (
+	<Layout>
+		<h1>About</h1>
+		<p>Rutters Barbary Coast furl trysail square-rigged aye clap of thunder rope's end quarterdeck Letter of Marque. Loot Sail ho salmagundi trysail smartly long clothes reef hearties brig Barbary Coast. No prey, no pay fathom yawl cackle fruit hogshead skysail six pounders yardarm heave down Sink me.</p>
+		<Link to="/">Home</Link>
+	</Layout>
+)
+
+export default AboutPage
